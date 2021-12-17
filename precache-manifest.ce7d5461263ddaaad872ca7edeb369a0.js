6aa920ff8b5e8f6d270268919f76c236461cce15
unknown <technicalbesmile@gmail.com>
2021-12-17 22:14:50 +0530

    Updates

---
+++ precache-manifest.ce7d5461263ddaaad872ca7edeb369a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eadbd923cde6a445e5a",
+    "url": "./static/js/main.7eadbd92.chunk.js"
+  },
+  {
+    "revision": "7b772b479e930e852685",
+    "url": "./static/js/1.7b772b47.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "85722abb00fea026ee8b1dbed31ea669",
+    "url": "./index.html"
+  }
+];
